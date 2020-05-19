178b42c987f80924291017aec490fd1f3aacc464
Simon <s1810238081@students.fh-hagenberg.at>
2020-05-19 11:59:19 +0200

    made button a spinny boi

--- components/ResetButton.js
+++ components/ResetButton.js
@@ -3,7 +3,8 @@ import styles from "./ResetButton.module.sass";
 export function ResetButton(props) {
     return (
         <div className={styles.resetButton} onClick={props.resetHandler}>
-            ↻ Reset
+            <div className={styles.spin}>↻</div>
+            <span>Reset</span>
         </div>
     );
 }
